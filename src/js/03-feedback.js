7b71cb5c73196ad11a3e5454752fc573cab8a463
htmldiana <dianekhomenko@gmail.com>
2023-02-21 16:46:37 +0100

    task-03 edit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,41 @@
-/*
-3. Під час сабміту форми очищуй сховище, 
-а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
-
-4. Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. 
-Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
-*/
-
+import { throttle } from 'lodash';
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', saveMessage);
+let savedData;
+let parsedData;
 
 updateValues();
 
-function saveMessage(evt) {
-  evt.preventDefault();
-  const userData = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
-  };
+form.addEventListener('input', throttle(e => {
+      e.preventDefault();
+      const userData = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+      };
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
-}
+    localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+    updateValues();
+}, 500)
+    
+);
 
 function updateValues() {
-    const savedData = localStorage.getItem('feedback-form-state');
-    const parsedData = JSON.parse(savedData)
+    savedData = localStorage.getItem('feedback-form-state');
+    parsedData = JSON.parse(savedData)
     
-    form.elements.email.value = parsedData.email;
-    form.elements.message.value = parsedData.message;
+    if (parsedData != null) {
+        form.elements.email.value = parsedData.email;
+        form.elements.message.value = parsedData.message;
+    }
 }
 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
+    console.log(parsedData);
     form.elements.email.value = '';
     form.elements.message.value = '';
-    console.log(parsedData);
 
     localStorage.clear();
 }
